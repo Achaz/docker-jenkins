98fe3a017daab65e5bccdfc5ca24064d89080d77
nidhi2802 <nidhigajjar2000@gmail.com>
2021-04-30 08:44:39 +0530

    edited java file for testing

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("Create a Jenkins CICD Pipeline to build a Docker Image with Splunk Integration");
+        System.out.println("Create a Jenkins CICD Pipeline to build a Docker Image with Splunk");
     }
 }
 
